94d93a150fb5521744fd1ebc1c86b0dbbf9932aa
Thimple01 <thimple01@gmail.com>
2022-03-28 05:44:34 +0900

    test

--- TestProject/src/JavaMain.java
+++ TestProject/src/JavaMain.java
@@ -4,6 +4,7 @@ public class JavaMain {
 	public static void main(String[] args) {
 		System.out.println("branch test 1");
 		System.out.println("branch test 1 append push");
+		System.out.println("branch test 1 append push 2");
 
 	}
 
